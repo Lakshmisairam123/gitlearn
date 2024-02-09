9221a07e215c41844d7124fef80763e5f9aa4711
Sairam <chowdarysairam5@gmail.com>
2024-02-09 11:42:34 -0500

    commiting only 2 files

---
+++ py.py
@@ -0,0 +1,9 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (sai)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print (sai)
+NameError: name 'sai' is not defined
+>>> print("sai")
+sai
